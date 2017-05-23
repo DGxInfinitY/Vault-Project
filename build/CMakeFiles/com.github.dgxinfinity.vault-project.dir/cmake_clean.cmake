2e5b7367ff6f211a7478484b747c0dfce674b7bb
Donovan Goodwin <ddg2goodwin@gmail.com>
2017-05-23 15:41:18 -0600

    Adding Make Build Files

---
+++ build/CMakeFiles/com.github.dgxinfinity.vault-project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "src/hello-again.c"
+  "com.github.dgxinfinity.vault-project_valac.stamp"
+  "CMakeFiles/com.github.dgxinfinity.vault-project.dir/src/hello-again.c.o"
+  "com.github.dgxinfinity.vault-project.pdb"
+  "com.github.dgxinfinity.vault-project"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/com.github.dgxinfinity.vault-project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
